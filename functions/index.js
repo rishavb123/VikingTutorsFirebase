36861dcb330bc7270ef935d7de8536ad07ce3a53
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-08 23:12:13 -0400

    created user verification and setup function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.createdUser = functions.auth.user().onCreate((user) => {
+    const domain = user.email.split("@")[1];
+    const name = user.displayName.split(" ");
+    if(domain !== "sbstudents.org" || domain !== "sbschools.org" || domain !== "bhagat.io")
+        return admin.auth().deleteUser(user.uid).then(() => console.log("Deleted Invalid User")).catch(e => console.log("Error:" + e));
+    else {
+        user.sendEmailVerification()
+        if(domain === "sbstudents.org")
+            return admin.firestore().collection("students").doc().set({
+                email: user.email,
+                name: {
+                    first: name[0],
+                    last: name[1]
+                },
+                uid: user.uid
+            });
+        else
+            return admin.firestore().collection("teachers").doc().set({
+                email: user.email,
+                name: {
+                    first: name[0],
+                    last: name[1]
+                },
+                uid: user.uid
+            });
+    }
+});

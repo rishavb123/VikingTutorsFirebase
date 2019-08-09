a4572a673d10660873bdbff12811d001923a1ba4
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-08-09 09:37:27 -0400

    removed anything with displayName since it is not set yet

--- functions/index.js
+++ functions/index.js
@@ -3,28 +3,13 @@ const admin = require("firebase-admin");
 
 exports.createdUser = functions.auth.user().onCreate((user) => {
     const domain = user.email.split("@")[1];
-    const name = user.displayName.split(" ");
     if(domain !== "sbstudents.org" || domain !== "sbschools.org" || domain !== "bhagat.io")
         return admin.auth().deleteUser(user.uid).then(() => console.log("Deleted Invalid User")).catch(e => console.log("Error:" + e));
     else {
         user.sendEmailVerification()
-        if(domain === "sbstudents.org")
-            return admin.firestore().collection("students").doc().set({
-                email: user.email,
-                name: {
-                    first: name[0],
-                    last: name[1]
-                },
-                uid: user.uid
-            });
-        else
-            return admin.firestore().collection("teachers").doc().set({
-                email: user.email,
-                name: {
-                    first: name[0],
-                    last: name[1]
-                },
-                uid: user.uid
-            });
+        return admin.firestore().collection(domain === "sbstudents.org"? "students" : "teachers").doc().set({
+            email: user.email,
+            uid: user.uid
+        });
     }
 });

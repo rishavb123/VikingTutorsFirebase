c8d1af989989c04b39aac6c56007fd9f5e5208a3
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-09-10 18:50:09 -0400

    working on function to transfer data

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
+const algoliasearch = require('algoliasearch');
+const axios = require('axios');
+
+const config = require('./config');
 
 admin.initializeApp();
 
@@ -19,4 +23,31 @@ exports.createdUser = functions.auth.user().onCreate((user) => {
                 return 0;
         });
     }
+});
+
+exports.moveData = functions.https.onRequest((req, res) => {
+    
+    let arr = [];
+    let datas = []
+
+    return admin.firestore().collection("videos").get().then((docs) => {
+        return docs.forEach((doc) => {
+            let data = doc.data();
+            data.id = doc.id;
+            datas.push(data);
+        })
+    }).then(() => {
+        
+        return Promise.all(datas.map(data => axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${data.videoID}&key=${config.YOUTUBE_API_KEY}`).then(resp => {
+            let video = {};
+            video.objectID = data.id;
+            video.title = resp.data.items[0].snippet.title;
+            video.description = resp.data.items[0].snippet.description;
+            return arr.push(video);
+        }).catch(e => console.log(e)))).then(() => {
+            return res.send(JSON.stringify(arr));
+        });
+        
+    });
+
 });
